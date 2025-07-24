7230826677aaedda7699bda13597ffdb5c7cba63
Russell <russellmorbey@hotmail.co.uk>
2025-07-24 22:56:21 +0100

    Added spaces between lines in about section.

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -5,9 +5,18 @@ function AboutCard({content}) {
   return (
     <Card className="quote-card-view">
       <Card.Body>
-        <blockquote className="blockquote mb-0">
+        <blockquote className="blockquote mb-0" >
           <p style={{ textAlign: "justify" }}>
-            {content?.fields.about_main_details}
+            {content?.fields.about_line_one}
+          </p>
+          <p style={{ textAlign: "justify" }}>
+            {content?.fields.about_line_two}
+          </p>
+          <p style={{ textAlign: "justify" }}>
+            {content?.fields.about_line_three}
+          </p>
+          <p style={{ textAlign: "justify" }}>
+            {content?.fields.about_line_four}
           </p>
         </blockquote>
       </Card.Body>
